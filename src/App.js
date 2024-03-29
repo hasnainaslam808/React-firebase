66d973ae80d4a098c071bf90cff384feaed269fa
Hasnain Aslam <hasnainaslam808@gmail.com>
2024-03-29 12:26:43 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 // import {getDatabase,ref,set} from "firebase/database";
 import {app} from "./firebase";
-
+import {getAuth, onAuthStateChanged, signOut} from "firebase/auth"
 import './App.css';
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import { useEffect, useState } from "react";
 
 // const db  = getDatabase();
-
+const auth=getAuth(app);
 function App() {
-
+const [user,setUser]=useState(null);
 // const putData = ()=>{
 //   set(ref(db,'users/hasnain'),{
 //     id:1,
@@ -17,16 +18,37 @@ function App() {
 //     age:22,
 //   })
 // }
+
+useEffect(()=>{
+  onAuthStateChanged(auth,user=>{
+    if(user){
+      setUser(user);
+    }
+    else{
+      setUser(null);
+    }
+  })
+},[])
+
+if(user === null){
+  return(
+    <div>
+    <h1>firebase </h1>
+    <Signup/>
+    <br></br>
+    <br></br>
+    <br></br>
+    <Login/>
+    {/* <button onClick={putData} >putData</button> */}
+      </div>
+  )
+}
   return (
     <div>
   <h1>firebase </h1>
-  <Signup/>
-  <br></br>
-  <br></br>
-  <br></br>
-  <br></br>
-  <br></br>
-  <Login/>
+<h1>hello{user.email}</h1>
+
+<button className="btn btn-warning" onClick={()=> signOut(auth)} >log out</button>
   {/* <button onClick={putData} >putData</button> */}
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,43 +1,51 @@
-import React ,{ useState }  from 'react'
-import {getAuth,signInWithEmailAndPassword} from "firebase/auth";
-import{app} from "../firebase"
+import React, { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase"
 
-const auth =getAuth(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
-    const [email,setEmail]= useState("");
-    const [password,setPassword]= useState("");
-   
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
 
-    const signin = () => {
+
+    const signin = (e) => {
+        e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
             .then(() => {
-              
+
                 alert("Login successful");
             })
             .catch((error) => {
-               
+
                 alert(error.message);
             });
+
+        
     }
-  return (
-  
+    const withGoogle = (e) => {
+        e.preventDefault();
+        signInWithPopup(auth, googleProvider)
+    }
+    return (
+
         <>
-        <h1>Log In</h1>
-            <form style={{margin:'20px'}}>
+            <h1>Log In</h1>
+            <form style={{ margin: '20px' }}>
                 <div className="form-group " >
                     <label >Email </label><br></br>
-                    <input onChange={(e)=>{setEmail(e.target.value)}} value={email} type="email"    placeholder="Enter email address"/>
-                       
+                    <input onChange={(e) => { setEmail(e.target.value) }} value={email} type="email" placeholder="Enter email address" />
+
                 </div>
                 <div className="form-group">
                     <label >Password</label><br></br>
-                    <input onChange={(e)=>{setPassword(e.target.value)}} value={password} type="password"   placeholder="Password"/>
+                    <input onChange={(e) => { setPassword(e.target.value) }} value={password} type="password" placeholder="Password" />
                 </div>
-             <br></br>
+                <br></br>
                 <button type="submit" className="btn btn-primary" onClick={signin}>Submit</button>
-                <button>Continue with google</button>
+                <button onClick={withGoogle} className='btn btn-success' >Continue with google</button>
             </form>
 
         </>
